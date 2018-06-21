8e477f943a7d139e193728b88c322739c93a936a
Clement Caira <clement.caira@supinternet.fr>
2018-06-21 10:27:39 +0200

    Working drawing method

--- app/src/main/java/com/caira/clement/littleDraw/DrawingView.java
+++ app/src/main/java/com/caira/clement/littleDraw/DrawingView.java
@@ -5,21 +5,25 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
+
 public class DrawingView extends View {
 
-
+    private boolean isSetup = false;
     private Path drawPath;
     private Paint drawPaint, canvasPaint;
     int paintColor = 0xFF660000;
     private Canvas drawCanvas;
     private Bitmap canvasBitmap;
 
-    public DrawingView(Context context) {
-        super(context);
+    public DrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
     }
+
+
 
     protected void setupDrawing() {
         drawPath = new Path();
@@ -33,6 +37,7 @@ public class DrawingView extends View {
         drawPaint.setStrokeCap(Paint.Cap.ROUND);
 
         canvasPaint = new Paint(Paint.DITHER_FLAG);
+        isSetup = true;
     }
 
     @Override
@@ -45,7 +50,36 @@ public class DrawingView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (!isSetup) {
+            setupDrawing();
+        }
         canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
         canvas.drawPath(drawPath, drawPaint);
+
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                break;
+            default:
+                return false;
+        }
+        invalidate();
+        return true;
     }
 }
