f3f9f1f5f63aa048d537b0646ac45aeffc17c6eb
Hakan Akca <hakan.akca@supinternet.fr>
2018-07-09 09:19:25 +0200

    Save

--- app/src/main/java/com/caira/clement/littleDraw/MainActivity.java
+++ app/src/main/java/com/caira/clement/littleDraw/MainActivity.java
@@ -1,22 +1,37 @@
 package com.caira.clement.littleDraw;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity implements ColorDialog.OnColorPickedListener {
 
-    ImageButton saveBtn;
+    private ImageButton saveBtn;
     ImageButton colorBtn;
     private ColorDialog colorDialog;
     private DrawingView drawingView;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +44,7 @@ public class MainActivity extends AppCompatActivity implements ColorDialog.OnCol
         colorBtn.setOnClickListener(colorPickerHandler);
 
         drawingView = findViewById(R.id.drawing);
+
     }
 
     View.OnClickListener colorPickerHandler = new View.OnClickListener() {
@@ -39,23 +55,44 @@ public class MainActivity extends AppCompatActivity implements ColorDialog.OnCol
         }
     };
 
+
+
     View.OnClickListener saveBtnHandler = new View.OnClickListener() {
+
         public void onClick(View v) {
             drawingView.setDrawingCacheEnabled(true);
-            String imgSaved = MediaStore.Images.Media.insertImage(
-                    getContentResolver(), drawingView.getDrawingCache(),
-                    UUID.randomUUID().toString()+".png", "drawing");
-            if(imgSaved!=null){
-                Toast savedToast = Toast.makeText(getApplicationContext(),
-                        "Drawing saved to Gallery!", Toast.LENGTH_SHORT);
-                savedToast.show();
+            OutputStream fOut = null;
+            Bitmap drawingBM = drawingView.getDrawingCache();
+            try {
+                if (drawingBM != null) {
+                    File mediaDirectory = new File(Environment.getExternalStorageDirectory().getPath()
+                            + "/DCIM");
+
+                    mediaDirectory.mkdir();
+                    File f = new File(mediaDirectory, UUID.randomUUID().toString() + ".png");
+                    fOut = new FileOutputStream(f);
+
+                    BufferedOutputStream bos = new BufferedOutputStream(fOut);
+
+                    drawingBM.compress(Bitmap.CompressFormat.PNG, 100, bos);
+
+                    bos.flush();
+                    bos.close();
+
+
+                    Toast savedToast = Toast.makeText(getApplicationContext(),
+                            "Drawing saved to Gallery!", Toast.LENGTH_SHORT);
+                    savedToast.show();
+                } else {
+
+                    Toast savedToast = Toast.makeText(getApplicationContext(),
+                            "NOPE !", Toast.LENGTH_SHORT);
+                    savedToast.show();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            else{
-                Toast unsavedToast = Toast.makeText(getApplicationContext(),
-                        "Oops! Image could not be saved.", Toast.LENGTH_SHORT);
-                unsavedToast.show();
-            }
-            drawingView.destroyDrawingCache();
         }
     };
 
